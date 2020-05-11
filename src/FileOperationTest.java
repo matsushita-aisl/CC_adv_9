2ba29dce6d520101e15b90f40e3353c09258ceeb
matsushita <matsushita@aisl.cs.tut.ac.jp>
2020-05-11 13:03:09 +0900

    add porocess for players

--- src/FileOperationTest.java
+++ src/FileOperationTest.java
@@ -18,8 +18,6 @@ public class FileOperationTest{
 	private static final Path DIR = Paths.get("./data/");	//Eclipseでの相対パス
 	private static final Path PLAYER = DIR.resolve("game_player.csv");	//DIRと連結 (In Eclipse, DAT's path is "${workspace}/${this project}/data/game_player.csv")
 	private static final Path OUTPUT = DIR.resolve("output.csv");	//DIRと連結 (In Eclipse, DAT's path is "${workspace}/${this project}/data/output.csv")
-	
-	static String str;
 	////
 	static String[] sample = {"1", "n", "j", "2", "3", "w", "a", "i", "", "", ""};
 	////
@@ -37,8 +35,9 @@ public class FileOperationTest{
 		//players.stream().forEach(x -> System.out.println(x + "," + Arrays.asList(x.getItems()).indexOf(null)));
 
 		for(Player p : players){
+			String[] items = p.getItems();
+			
 			if(p.getLevel() == 1){
-				String[] items = p.getItems();
 				if(Arrays.asList(items).contains(null)){
 					items[Arrays.asList(p.getItems()).indexOf(null)] = "応援旗";
 					p.setItems(items);
@@ -46,11 +45,20 @@ public class FileOperationTest{
 					p.setPossetion(p.getPossetion() + 100);
 				}
 			}
+			
+			if(p.getLevel() <= 10){
+				p.setPossetion(p.getPossetion() + 100);
+			}
+			
+			if(Arrays.asList(items).contains("鋼玉")){
+				items[Arrays.asList(items).indexOf("鋼玉")] = "スペシャルソード";
+			}
 		}
 	}
 	
 	static List<Player> load(Path FILE){
 		List<Player> players = new ArrayList<Player>();
+		String str;
 		
 		try(BufferedReader bw = Files.newBufferedReader(FILE)){	//CSVの読み込み
 			
